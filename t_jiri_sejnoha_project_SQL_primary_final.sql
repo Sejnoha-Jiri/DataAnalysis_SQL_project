fb929a5abdba04c83c658dd0b62ae5481b7bbc3a
Sejnoha-Jiri <147030474+Sejnoha-Jiri@users.noreply.github.com>
2023-11-02 11:43:47 +0100

    Update t_jiri_sejnoha_project_SQL_primary_final.sql
    finishing each statement with ;

--- t_jiri_sejnoha_project_SQL_primary_final.sql
+++ t_jiri_sejnoha_project_SQL_primary_final.sql
@@ -16,7 +16,7 @@
         JOIN engeto.czechia_payroll_unit cpu ON cpu.code = cp.unit_code 
         JOIN engeto.czechia_payroll_value_type cpvt ON cpvt.code = cp.value_type_code
         )
-        
+        ;
     --Price data
         CREATE OR REPLACE TEMPORARY TABLE engeto.czechia_price_decoded (
         SELECT
@@ -32,7 +32,7 @@
         JOIN engeto.czechia_price_category cpc ON cpc.code = cp.category_code
         JOIN engeto.czechia_region cr ON cr.code = cp.region_code 
         )
-
+        ;
 --Adjusting decoded tables for more specificity
     --Wages
         -- Creating value for difference between wages per industry branch per year
@@ -50,7 +50,7 @@
                 industry_branch,
                 payroll_year
             )
-
+            ;
             -- Listing wages per industry branch per year
             CREATE OR REPLACE TEMPORARY TABLE engeto.wage_per_branch_year (
             SELECT
@@ -60,6 +60,7 @@
                 ROUND(COALESCE(((wage / NULLIF(LAG(wage, 1) OVER (PARTITION BY industry_branch ORDER BY payroll_year), 0)) - 1) * 100,0),3) AS wage_increase
             FROM engeto.mid_result_wage_per_branch_year
             )
+            ;
     --Prices
         -- Listing numbers of rows to determine first and last recorded years
             CREATE OR REPLACE TEMPORARY TABLE engeto.mid_result_price_per_category_year(
@@ -75,7 +76,7 @@
                 category,
                 price_year
             )
-
+            ;
         -- Listing prices per unit per category per year
             CREATE OR REPLACE TEMPORARY TABLE engeto.price_per_category_year(
             SELECT
@@ -88,7 +89,7 @@
                 ROUND(COALESCE(((price_per_unit / NULLIF(LAG(price_per_unit, 1) OVER (PARTITION BY category ORDER BY price_year), 0)) - 1) * 100,0),3) AS price_increase
             FROM engeto.mid_result_price_per_category_year
             )
-
+            ;
 -- Uniting for final table, it will be possible to extract all data from this table, but I choose to work with them separately as it will be more readable in my opinion.
     CREATE OR REPLACE TABLE engeto.t_jiri_sejnoha_project_SQL_primary_final AS
     SELECT
@@ -118,10 +119,11 @@
         last_year,
         price_increase
     FROM engeto.price_per_category_year;
-
+    ;
 -- Isolating relevant data from GDP
     CREATE OR REPLACE TABLE engeto.t_jiri_sejnoha_project_SQL_secondary_final (
     SELECT *
     FROM engeto.economies
     WHERE country LIKE '%Czech Republic%' AND `year` BETWEEN  2005 AND 2016
     )
+    ;
